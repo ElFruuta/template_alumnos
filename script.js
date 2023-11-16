0e364e0f188b21ac407c2b1b97803a3841427a88
ElFruutas <yastin.villarroel.cancino@alumnos.sip.cl>
2023-11-16 10:21:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+// INSTRUCCIONES:
+// 1. El objetivo de este ejercicio es crear un programa que simule una bola 8 mágica. 
+// El programa debe permitir al usuario ingresar una pregunta y mostrar una respuesta aleatoria entre las siguientes opciones:
+// "No", "Sí", "No lo creo...", "¡Por supuesto!", "Indudablemente", "En tus sueños."
+
+var respuestas = ["No","Si","No lo creo...","¡Por supuesto!", "Indudablemente", "En tus sueños."];
+
+function generarRespuesta(pregunta) {
+  var respuestaAleatoria = respuestas[Math.floor(Math.random() * respuestas.length)];
+  console.log("Pregunta: " + pregunta);
+  console.log("Respuesta: " + respuestaAleatoria);
+}
+ // Prueba
+generarRespuesta("¿Tendré un buen día de hoy?");
+generarRespuesta("¿Ganaré la lotería este fin de semana?");
+
+// 2. Puede utilizar todo lo aprendido hasta ahora para resolver este ejercicio, 
+// no puede hace uso de librerías de código externas y la copia será penalizada de acuerdo al reglamento.
+
+// 3. Hay ciertas líneas de código que      no debe modificar, 
+// pero puede agregar todo el código que necesite para resolver el ejercicio.
+// y puede mover estas líneas hacia donde crea que es necesario para que el programa funcione correctamente.
+
+// 4. El programa debe funcionar de la siguiente manera:
+// 4.1. Luego de hacer click en el botón de pregunta, el programa debe mostrar un prompt que le pida al usuario que ingrese una pregunta.
+// 4.2. El usuario debe ingresar una pregunta a hacerle a la bola y esta pregunta debe mostrarse en consola o en la página, donde usted crea que es mejor.
+// 4.3. Se debe generar una respuesta aleatora, de acuerdo a las opciones que se muestran en el punto 1. 
+// 4.4. Después de 500 milisegundos, el programa debe cambiar el texto de la respuesta donde se señala en el html.
+// (debe mostrar la respuesta en el centro de la imagen de la respuesta) junto con la imagen de "respuesta".
+
+// 5. DEBE COMENTAR SU CÓDIGO, EXPLICANDO PARA QUE SIRVE CADA PARTE DEL MISMO.
+
+
+// BONUS: Hacer un movimiento de "agitado" de la bola 8, antes de mostrar la respuesta.
+
+
+
+document.addEventListener("DOMContentLoaded", function() { // NO ELIMINAR
+  // Estas son las url de las imágenes que se usan en el programa, cuando quiera mostrar una respuesta, uso la imagen de la respuesta, 
+  // cuando quiera mostrar el 8, uso la imagen de la pregunta.
+  const imagen_respuesta = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballAnswer.png";
+  const imagen_pregunta = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png";
+
+  // ESCRIBE TU CODIGO AQUI
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+  // AYUDA: ESA ES LA ALERTA QUE PERMITE RECIBIR UNA PREGUNTA DEL USUARIO, LA PREGUNTA SE GUARDA EN LA VARIABLE pregunta  
+  var pregunta = prompt("ASK A YES/NO QUESTION!");
+
+});
